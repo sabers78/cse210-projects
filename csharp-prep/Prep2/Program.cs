dfb0244f2961deacdbe53abb9e94849131e3b1c5
Nicolas Hopkins <seekerking123@gmail.com>
2024-04-26 18:57:38 -0600

    I finished my program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage");
+        string grade = Console.ReadLine();
+
+        int percent = int.Parse(grade);
+
+        string letter = "null";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else if (percent < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (percent >= 70)
+        {
+            Console.Write("You passed the class! Great job!");
+        }
+        else
+        {
+            Console.Write("You failed the class. Don't give up, though!");
+        }
+
     }
 }
