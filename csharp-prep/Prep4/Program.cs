f567bd0f60d5cf41251f340a09adf92d680419f2
Nicolas Hopkins <seekerking123@gmail.com>
2024-05-03 23:17:43 -0600

    I actually did it. Yay!

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,55 @@
 using System;
 
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        int userNumber = int.Parse("2");
+        
+        List<int> numberList = new List<int>();
+
+
+        while (userNumber != 0)
+        {
+            Console.WriteLine("enter one number one at a time. enter 0 when you want to stop");
+            userNumber = int.Parse(Console.ReadLine());
+            
+            
+            if (userNumber != 0)
+            {
+                numberList.Add(userNumber);
+            }    
+        }
+        int blankSum = int.Parse("0");
+        foreach (int answer in numberList)
+        {
+            blankSum += answer;
+        }
+
+        int answerSize = int.Parse("0");
+        foreach (int answer in numberList)
+        {
+            answerSize = numberList.Count;
+        }
+        float average = (((float)blankSum) / answerSize);
+
+
+        int largestNumber = int.Parse("0");
+        foreach (int answer in numberList)
+        {
+            if (answer >= largestNumber)
+            {
+                largestNumber = answer;
+            }
+        }
+
+        Console.WriteLine($"The sum is {blankSum}");
+
+        Console.WriteLine($"The average is {average:0.00##}");
+
+        Console.WriteLine($"The largest number is {largestNumber}");
+    }   
 }
