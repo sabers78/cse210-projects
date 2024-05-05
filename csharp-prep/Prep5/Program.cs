a06012a69da6ebe1a14dd03384af863fc02c14e8
Nicolas Hopkins <seekerking123@gmail.com>
2024-05-04 22:13:15 -0600

    I can't belive I actually understood what was going on.... Sweeeeet.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
-    }
+
+        DisplayWelcome();
+        string futureName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int finishedSqaure = SquareNumber(userNumber);
+        DisplayResult(futureName,finishedSqaure);
+
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+
+        }
+
+        static string PromptUserName()
+        {
+            Console.WriteLine("What is your name?");
+            string username = Console.ReadLine();
+
+            return username;
+        }
+
+        static int PromptUserNumber()
+        {
+            
+            Console.WriteLine("What is your favorite number?");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            return userNumber;
+
+        }
+
+        static int SquareNumber(int userNumber)
+        {
+            int yourSquare = userNumber * userNumber;
+
+            return yourSquare;
+        }
+
+        static void DisplayResult(string username, int yourSqaure)
+        {
+            Console.WriteLine($"{username}, the sqaure of your number is {yourSqaure}.");
+        }
+    }    
 }
